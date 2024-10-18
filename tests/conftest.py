046ce0b0fdcb4773df609d66a22d898c2aa9a4c1
Juan Fonseca <juan.fonseca@gorillalogic.com>
2024-10-18 09:01:08 -0600

    Added webdriver manager.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 import pytest
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture()
 def driver():
-    my_driver = webdriver.Chrome()
+    my_driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install())
+    )
     yield my_driver
     my_driver.quit()
