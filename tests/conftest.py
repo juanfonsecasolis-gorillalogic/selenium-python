113bae446da94291785b9d185f101f01f456918c
Juan Fonseca <juan.fonseca@gorillalogic.com>
2024-10-18 09:24:21 -0600

    Added webdriver manager.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,14 @@ from selenium import webdriver
 import pytest
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture()
 def driver():
+    options = Options()
+    options.add_argument('--no-sandbox')
     my_driver = webdriver.Chrome(
+        options=options,
         service=Service(ChromeDriverManager().install())
     )
     yield my_driver
